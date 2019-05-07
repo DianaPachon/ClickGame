616e1df70dc35c514bda9602cb2138ada04892a8
Lady Diana <dianapachon2017@gmail.com>
2019-05-07 18:38:41 -0400

    Updates

---
+++ precache-manifest.24b5516da24efabc473a679110cd132f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "569fc8db04d8bba144a6286bca189d1e",
+    "url": "/ClickGame/index.html"
+  },
+  {
+    "revision": "3d2a9174ffcb3c65bd9f",
+    "url": "/ClickGame/static/css/main.a0870639.chunk.css"
+  },
+  {
+    "revision": "3806bbef38d18113e6e5",
+    "url": "/ClickGame/static/js/2.b35c4eba.chunk.js"
+  },
+  {
+    "revision": "3d2a9174ffcb3c65bd9f",
+    "url": "/ClickGame/static/js/main.8ce47cb6.chunk.js"
+  },
+  {
+    "revision": "d76327d9eefc62a77850",
+    "url": "/ClickGame/static/js/runtime~main.979c4616.js"
+  },
+  {
+    "revision": "3263431daa539574f8938ae0adc9e063",
+    "url": "/ClickGame/static/media/deer.3263431d.jpg"
+  },
+  {
+    "revision": "5d69c87043024d5a95b3d4b0244a0590",
+    "url": "/ClickGame/static/media/elephant.5d69c870.jpg"
+  },
+  {
+    "revision": "fd98199cf6bc9f03415ed3942663466c",
+    "url": "/ClickGame/static/media/flamingo.fd98199c.png"
+  },
+  {
+    "revision": "fb591c02bf14346083a1e9f93947b56f",
+    "url": "/ClickGame/static/media/giraffe.fb591c02.png"
+  },
+  {
+    "revision": "166aa2ecb098a7d3a710bf1ac75b9767",
+    "url": "/ClickGame/static/media/koala.166aa2ec.png"
+  },
+  {
+    "revision": "f3de4c91046b7724fe95dfe19db59df4",
+    "url": "/ClickGame/static/media/lion.f3de4c91.png"
+  },
+  {
+    "revision": "05e5fcad8696e526a593ccfffca5901c",
+    "url": "/ClickGame/static/media/monkey.05e5fcad.png"
+  },
+  {
+    "revision": "b700cfd902bee46c65665bff84ca7bc6",
+    "url": "/ClickGame/static/media/parrot.b700cfd9.jpg"
+  },
+  {
+    "revision": "d475c9a8f87105eb1f88f64322949f80",
+    "url": "/ClickGame/static/media/zebra.d475c9a8.png"
+  }
+]);
